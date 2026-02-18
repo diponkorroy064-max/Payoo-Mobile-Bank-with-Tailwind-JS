f61ab13a0c4bfbb699da62f74fd141ae7e393d3a
diponkorroy064-max <diponkorroy064@gmail.com>
2026-02-19 01:01:07 +0600

    add home file

--- script/login.js
+++ script/login.js
@@ -1,1 +1,24 @@
+// console.log('login function');
+
+document.getElementById('login-btn').addEventListener('click', function () {
+    // get the mobile number
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+
+    // get the pin
+    const inputPin = document.getElementById('input-pin');
+    const pinNumber = inputPin.value;
+
+    // match pin and mobile number
+    if (contactNumber === "01234567891" && pinNumber === "1234") {
+        // true-> alert---->homepage
+        alert("LogIn Successful");
+        window.location.assign("/home.html");
+    }
+    else {
+        // false-> alert------> return
+        alert("Login failed");
+        return;
+    }
+});
 
